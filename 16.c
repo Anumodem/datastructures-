69cbe18fdacfff4c8449d6c98ed2c168e526e1a6
Anumodem <110585951+Anumodem@users.noreply.github.com>
2023-01-29 17:00:33 +0530

    c

---
+++ 16.c
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void ss()
+{
+return;
+}
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for (int i = 0; i < n; ++i)
+{
+cin>>arr[i];
+}
+int m;
+cin>>m;
+cout<<"Linked List:";
+for(int p : arr)
+{
+if(p != m)
+cout<<"->"<<p;
+}
+return 0;
+cout<<"struct node node *next; void create() p2=p2->next; void del()";
+}
